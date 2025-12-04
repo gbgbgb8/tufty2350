b4d5c55a32087f68a333dc009f756265a4f295c3
thirdr <ryan@pimoroni.com>
2025-12-04 12:20:34 +0000

    Apps: adding new badge app

--- firmware/apps/badge/__init__.py
+++ firmware/apps/badge/__init__.py
@@ -1,361 +1,224 @@
 import sys
 import os
+import math
+from badgeware import run, WIDTH, HEIGHT, clamp
 
 sys.path.insert(0, "/system/apps/badge")
 os.chdir("/system/apps/badge")
 
 
-from badgeware import io, brushes, shapes, Image, run, PixelFont, screen, Matrix, file_exists
-import random
-import math
-import network
-from urllib.urequest import urlopen
-import gc
-import sys
-import json
+CX = WIDTH / 2
+CY = HEIGHT / 2
+
+screen.antialias = screen.X2
+
+# details to be shown on the card
+id_photo = Image.load("avatar.png")
+id_name = "Your Name"
+id_role = "Job title"
+
+# see the 'assets/social' folder to see what's supported
+id_socials = {"bluesky": {"icon": None, "handle": ""},
+              "instagram": {"icon": None, "handle": ""},
+              "github": {"icon": None, "handle": ""},
+              "discord": {"icon": None, "handle": ""}
+              }
+
+# load in the social icons
+for key in id_socials.keys():
+    id_socials[key]["icon"] = Image.load(f"assets/socials/{key}.png")
+
+# id card variables
+id_body = shapes.rounded_rectangle(0, 0, 140, 100, 7)
+id_outline = shapes.rounded_rectangle(0, 0, 140, 100, 7).stroke(2)
+hue = 0.0
+saturation = 0.0
+background = brushes.color(255, 255, 255)
+flip = False
+flip_start = 0
+rear_view = False
+card_pos = (10, 10)
+
+small_font = PixelFont.load("/system/assets/fonts/winds.ppf")
+large_font = PixelFont.load("/system/assets/fonts/nope.ppf")
 
 
-phosphor = brushes.color(211, 250, 55, 150)
-white = brushes.color(235, 245, 255)
-faded = brushes.color(235, 245, 255, 100)
-small_font = PixelFont.load("/system/assets/fonts/ark.ppf")
-large_font = PixelFont.load("/system/assets/fonts/absolute.ppf")
+# Source - https://stackoverflow.com/a
+# Posted by Tcll, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-12-04, License - CC BY-SA 4.0
 
-WIFI_TIMEOUT = 60
-CONTRIB_URL = "https://github.com/{user}.contribs"
-USER_AVATAR = "https://wsrv.nl/?url=https://github.com/{user}.png&w=75&output=png"
-DETAILS_URL = "https://api.github.com/users/{user}"
-
-WIFI_PASSWORD = None
-WIFI_SSID = None
-
-wlan = None
-connected = False
-ticks_start = None
+scalar = 1.0
 
 
-def message(text):
-    print(text)
+def hsv_to_rgb(h: scalar, s: scalar, v: scalar, a: scalar) -> tuple:
+    a = int(255 * a)
+    if s:
+        if h == 1.0:
+            h = 0.0
+        i = int(h * 6.0)
+        f = h * 6.0 - i
+
+        w = int(255 * (v * (1.0 - s)))
+        q = int(255 * (v * (1.0 - s * f)))
+        t = int(255 * (v * (1.0 - s * (1.0 - f))))
+        v = int(255 * v)
+
+        if i == 0:
+            return (v, t, w, a)
+        if i == 1:
+            return (q, v, w, a)
+        if i == 2:
+            return (w, v, t, a)
+        if i == 3:
+            return (w, q, v, a)
+        if i == 4:
+            return (t, w, v, a)
+        if i == 5:
+            return (v, w, q, a)
+    else:
+        v = int(255 * v)
+        return (v, v, v, a)
 
 
-def get_connection_details(user):
-    global WIFI_PASSWORD, WIFI_SSID
+def draw_background():
+    # ripple effect background
+    cy = CY - 8
+    cx = CX
 
-    if WIFI_SSID is not None and user.handle is not None:
-        return True
-
-    try:
-        sys.path.insert(0, "/")
-        from secrets import WIFI_PASSWORD, WIFI_SSID, GITHUB_USERNAME
-        sys.path.pop(0)
-    except ImportError:
-        WIFI_PASSWORD = None
-        WIFI_SSID = None
-        GITHUB_USERNAME = None
-
-    if not WIFI_SSID:
-        return False
-
-    if not GITHUB_USERNAME:
-        return False
-
-    user.handle = GITHUB_USERNAME
-
-    return True
+    y = 0
+    for row in range(12):
+        x = 0
+        for col in range(16):
+            dist = math.sqrt((x + 5 - cx) ** 2 + (y + 5 - cy) ** 2)
+            pulse = (math.sin(-io.ticks / 400 + (dist / 6)) / 2) + 0.5
+            pulse = 0.8 + (pulse / 2)
+            screen.brush = brushes.color(0, 0, 0, 100 * pulse)
+            screen.rectangle(x, y, 10, 10)
+            x += 10
+        y += 10
 
 
-def wlan_start():
-    global wlan, ticks_start, connected, WIFI_PASSWORD, WIFI_SSID
-
-    if ticks_start is None:
-        ticks_start = io.ticks
-
-    if connected:
-        return True
-
-    if wlan is None:
-        wlan = network.WLAN(network.STA_IF)
-        wlan.active(True)
-
-        if wlan.isconnected():
-            return True
-
-        wlan.connect(WIFI_SSID, WIFI_PASSWORD)
-
-        print("Connecting to WiFi...")
-
-    connected = wlan.isconnected()
-
-    if io.ticks - ticks_start < WIFI_TIMEOUT * 1000:
-        if connected:
-            return True
-    elif not connected:
-        return False
-
-    return True
-
-
-def async_fetch_to_disk(url, file, force_update=False):
-    if not force_update and file_exists(file):
-        return
-    try:
-        # Grab the data
-        response = urlopen(url, headers={"User-Agent": "GitHub Universe Badge 2025"})
-        data = bytearray(512)
-        total = 0
-        with open(file, "wb") as f:
-            while True:
-                if (length := response.readinto(data)) == 0:
-                    break
-                total += length
-                message(f"Fetched {total} bytes")
-                f.write(data[:length])
-                yield
-        del data
-        del response
-    except Exception as e:
-        raise RuntimeError(f"Fetch from {url} to {file} failed. {e}") from e
-
-
-def get_user_data(user, force_update=False):
-    message(f"Getting user data for {user.handle}...")
-    yield from async_fetch_to_disk(DETAILS_URL.format(user=user.handle), "/user_data.json", force_update)
-    r = json.loads(open("/user_data.json", "r").read())
-    user.name = r["name"]
-    user.handle = r["login"]
-    user.followers = r["followers"]
-    user.repos = r["public_repos"]
-    del r
-    gc.collect()
-
-
-def get_contrib_data(user, force_update=False):
-    message(f"Getting contribution data for {user.handle}...")
-    yield from async_fetch_to_disk(CONTRIB_URL.format(user=user.handle), "/contrib_data.json", force_update)
-    r = json.loads(open("/contrib_data.json", "r").read())
-    user.contribs = r["total_contributions"]
-    user.contribution_data = [[0 for _ in range(53)] for _ in range(7)]
-    for w, week in enumerate(r["weeks"]):
-        for day in range(7):
-            try:
-                user.contribution_data[day][w] = week["contribution_days"][day]["level"]
-            except IndexError:
-                pass
-    del r
-    gc.collect()
-
-
-def get_avatar(user, force_update=False):
-    message(f"Getting avatar for {user.handle}...")
-    yield from async_fetch_to_disk(USER_AVATAR.format(user=user.handle), "/avatar.png", force_update)
-    user.avatar = Image.load("/avatar.png")
-
-
-def fake_number():
-    return random.randint(10000, 99999)
-
-
-def placeholder_if_none(text):
-    if text:
-        return text
-    old_seed = random.seed()
-    random.seed(int(io.ticks / 100))
-    chars = "!\"£$%^&*()_+-={}[]:@~;'#<>?,./\\|"
-    text = ""
-    for _ in range(20):
-        text += random.choice(chars)
-    random.seed(old_seed)
-    return text
-
-
-class User:
-    levels = [
-        brushes.color(21 / 2,  27 / 2,  35 / 2),
-        brushes.color(3 / 2,  58 / 2,  22 / 2),
-        brushes.color(25 / 2, 108 / 2,  46 / 2),
-        brushes.color(46 / 2, 160 / 2,  67 / 2),
-        brushes.color(86 / 2, 211 / 2, 100 / 2),
-    ]
-
-    def __init__(self):
-        self.handle = None
-        self.update()
-
-    def update(self, force_update=False):
-        self.name = None
-        self.followers = None
-        self.contribs = None
-        self.contribution_data = None
-        self.repos = None
-        self.avatar = None
-        self._task = None
-        self._force_update = force_update
-
-    def draw_stat(self, title, value, x, y):
-        screen.brush = white if value else faded
-        screen.font = large_font
-        screen.text(str(value) if value is not None else str(fake_number()), x, y)
-        screen.font = small_font
-        screen.brush = phosphor
-        screen.text(title, x - 1, y + 13)
-
-    def draw(self, connected):
-        # draw contribution graph background
-        size = 15
-        graph_width = 53 * (size + 2)
-        xo = int(-math.sin(io.ticks / 5000) *
-                 ((graph_width - 160) / 2)) + ((graph_width - 160) / 2)
-
-        screen.font = small_font
-        rect = shapes.rounded_rectangle(0, 0, size, size, 2)
-        for y in range(7):
-            for x in range(53):
-                if self.contribution_data:
-                    level = self.contribution_data[y][x]
-                    screen.brush = User.levels[level]
-                else:
-                    screen.brush = User.levels[1]
-                pos = (x * (size + 2) - xo, y * (size + 2) + 1)
-                if pos[0] + size < 0 or pos[0] > 160:
-                    # skip tiles that aren't in view
-                    continue
-                rect.transform = Matrix().translate(*pos)
-                screen.draw(rect)
-
-        # draw handle
-        screen.font = large_font
-        handle = self.handle
-
-        # use the handle area to show loading progress if not everything is ready
-        if (not self.handle or not self.avatar or not self.contribs) and connected:
-            if not self.name:
-                handle = "fetching user data..."
-                if not self._task:
-                    self._task = get_user_data(self, self._force_update)
-            elif not self.contribs:
-                handle = "fetching contribs..."
-                if not self._task:
-                    self._task = get_contrib_data(self, self._force_update)
-            else:
-                handle = "fetching avatar..."
-                if not self._task:
-                    self._task = get_avatar(self, self._force_update)
-
-            try:
-                next(self._task)
-            except StopIteration:
-                self._task = None
-
-        if not connected:
-            handle = "connecting..."
-
-        w, _ = screen.measure_text(handle)
-        screen.brush = white
-        screen.text(handle, 80 - (w / 2), 2)
-
-        # draw name
-        screen.font = small_font
-        screen.brush = phosphor
-        name = placeholder_if_none(self.name)
-        w, _ = screen.measure_text(name)
-        screen.text(name, 80 - (w / 2), 16)
-
-        # draw statistics
-        self.draw_stat("followers", self.followers, 88, 33)
-        self.draw_stat("contribs", self.contribs, 88, 62)
-        self.draw_stat("repos", self.repos, 88, 91)
-
-        # draw avatar imagee
-        if not self.avatar:
-            # create a spinning loading animation while we wait for the avatar to load
-            screen.brush = phosphor
-            squircle = shapes.squircle(0, 0, 10, 5)
-            screen.brush = brushes.color(211, 250, 55, 50)
-            for i in range(4):
-                mul = math.sin(io.ticks / 1000) * 14000
-                squircle.transform = Matrix().translate(42, 75).rotate(
-                    (io.ticks + i * mul) / 40).scale(1 + i / 1.3)
-                screen.draw(squircle)
-        else:
-            screen.blit(self.avatar, 5, 37)
-
-
-user = User()
-connected = file_exists("/contrib_data.json") and file_exists("/user_data.json") and file_exists("/avatar.png")
-force_update = False
+def shadow_text(text, x, y):
+    screen.brush = brushes.color(20, 40, 60, 100)
+    screen.text(text, x + 1, y + 1)
+    screen.brush = brushes.color(0, 0, 0)
+    screen.text(text, x, y)
 
 
 def center_text(text, y):
-  w, h = screen.measure_text(text)
-  screen.text(text, 80 - (w / 2), y)
+    w, _ = screen.measure_text(text)
+    shadow_text(text, 80 - (w / 2), y)
 
 
-def wrap_text(text, x, y):
-  lines = text.splitlines()
-  for line in lines:
-    _, h = screen.measure_text(line)
-    screen.text(line, x, y)
-    y += h * 0.8
+def init():
+    pass
 
 
-# tell the user where to fill in their details
-def no_secrets_error():
-  screen.font = large_font
-  screen.brush = white
-  center_text("Missing Details!", 5)
+def change_background(h=None, s=None):
+    # a little helper to change the background color
+    global background, hue, saturation
 
-  screen.text("1:", 10, 23)
-  screen.text("2:", 10, 55)
-  screen.text("3:", 10, 87)
+    if h:
+        hue += h
+        hue = hue % 1
+        rgb = hsv_to_rgb(hue, saturation, 1.0, 1.0)
+        background = brushes.color(*rgb)
 
-  screen.brush = phosphor
-  screen.font = small_font
-  wrap_text("""Put your badge into\ndisk mode (tap\nRESET twice)""", 30, 24)
-
-  wrap_text("""Edit 'secrets.py' to\nset WiFi details and\nGitHub username.""", 30, 56)
-
-  wrap_text("""Reload to see your\nsweet sweet stats!""", 30, 88)
-
-
-# tell the user that the connection failed :-(
-def connection_error():
-  screen.font = large_font
-  screen.brush = white
-  center_text("Connection Failed!", 5)
-
-  screen.text("1:", 10, 63)
-  screen.text("2:", 10, 95)
-
-  screen.brush = phosphor
-  screen.font = small_font
-  wrap_text("""Could not connect\nto the WiFi network.\n\n:-(""", 16, 20)
-
-  wrap_text("""Edit 'secrets.py' to\nset WiFi details and\nGitHub username.""", 30, 65)
-
-  wrap_text("""Reload to see your\nsweet sweet stats!""", 30, 96)
+    if s:
+        saturation += s
+        saturation = clamp(saturation, 0.0, 1.0)
+        rgb = hsv_to_rgb(hue, saturation, 1.0, 1.0)
+        background = brushes.color(*rgb)
 
 
 def update():
-    global connected, force_update
+    global flip, flip_start, rear_view, background_hue, background
 
-    screen.brush = brushes.color(0, 0, 0)
-    screen.draw(shapes.rectangle(0, 0, 160, 120))
+    # unpack the x and y for the card
+    x, y = card_pos
 
-    force_update = False
+    width = 1
 
-    if io.BUTTON_A in io.held and io.BUTTON_C in io.held:
-        connected = False
-        user.update(True)
+    # clear the screen
+    screen.brush = background
+    screen.clear()
 
-    if get_connection_details(user):
-        if wlan_start():
-            user.draw(connected)
-        else:  # Connection Failed
-            connection_error()
-    else:      # Get Details Failed
-        no_secrets_error()
+    # ripple effect
+    draw_background()
+
+    if io.BUTTON_B in io.pressed:
+        flip = True
+        flip_start = io.ticks
+        rear_view = not rear_view
+
+    if io.BUTTON_UP in io.held:
+        change_background(h=0.01)
+
+    if io.BUTTON_DOWN in io.held:
+        change_background(h=-0.01)
+
+    if io.BUTTON_C in io.held:
+        change_background(s=0.1)
+
+    if io.BUTTON_A in io.held:
+        change_background(s=-0.1)
+
+    if flip:
+        # create a spin animation that runs over 100ms
+        speed = 90
+        frame = io.ticks - flip_start
+
+        # calculate the width of the tile during this part of the animation
+        width = round(math.cos(frame / speed) * 3) / 3
+
+        # ensure the width never reduces to zero or the icon disappears
+        width = max(0.1, width) if width > 0 else min(-0.1, width)
+
+        # once the animation has completed unset the spin flag
+        if frame > (speed * 3):
+            flip = False
+
+    # draw the card
+    id_body.transform = Matrix().translate(CX, y).scale(width, 1)
+    id_outline.transform = Matrix().translate(CX, y).scale(width, 1)
+    id_body.transform = id_body.transform.translate(-70, 0)
+    id_outline.transform = id_outline.transform.translate(-70, 0)
+
+    screen.brush = brushes.color(50, 50, 50, 100)
+    id_body.transform = id_body.transform.translate(4, 4)
+    screen.draw(id_body)
+
+    screen.brush = brushes.color(255, 255, 255, 90)
+    id_body.transform = id_body.transform.translate(-4, -4)
+    screen.draw(id_body)
+    screen.brush = brushes.color(0, 0, 0, 100)
+    screen.draw(id_outline)
+
+    photo_y = y + 15 + id_photo.height
+    socials_y = 22
+
+    if not flip:
+        # Draw the card information
+        screen.brush = brushes.color(0, 0, 0)
+        if not rear_view:
+            screen.font = large_font
+            screen.blit(id_photo, CX - id_photo.width / 2, y + 10)
+            center_text(id_name, photo_y)
+            screen.font = small_font
+            center_text(id_role, photo_y + 12)
+        else:
+            screen.font = large_font
+            for account in id_socials.items():
+                screen.brush = brushes.color(100, 100, 100)
+                screen.draw(shapes.rounded_rectangle(20, socials_y, 17, 17, 3))
+                screen.blit(account[1]["icon"], 20, socials_y)
+                shadow_text(account[1]["handle"], 40, socials_y)
+                socials_y += 21
+
+
+def on_exit():
+    pass
 
 
 if __name__ == "__main__":
     run(update)
-
